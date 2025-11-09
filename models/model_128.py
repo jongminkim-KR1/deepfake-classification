3d19898d078e3074be45c716b99d18320b301dfd
deneb784 <deneb784@gmail.com>
2025-11-10 08:56:39 +0900

    train base 함수 추가

--- models/model_128.py
+++ models/model_128.py
@@ -1,23 +1,15 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from base_models import fft_2d
 
-def fft_2d(img: torch.Tensor):
-    fft_result = torch.fft.fft2(img)
-
-    fft_shifted = torch.fft.fftshift(fft_result)
-
-    magnitude = torch.abs(fft_shifted)
-    phase = torch.angle(fft_shifted)
-    return magnitude, phase
-
-class CNNClfWithFFT(nn.Module):
+class CNNClfWithFFT128(nn.Module):
     def __init__(self, num_classes : int = 2):
-        super(CNNClfWithFFT, self).__init__()
+        super(CNNClfWithFFT128, self).__init__()
         input_size = 512
-        layer_size = (32, 64, 64, 32)
-        filter_size = (7, 5, 3)
-        padding_size = (3, 2, 1)
+        layer_size = (64, 64, 128, 32)
+        filter_size = (7, 5, 3, 3)
+        padding_size = (3, 2, 1, )
 
         self.rgb_branch = nn.Sequential(
             nn.Conv2d(3, layer_size[0], kernel_size=filter_size[0], stride=1, padding=padding_size[0]),
@@ -29,13 +21,13 @@ class CNNClfWithFFT(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(layer_size[1], layer_size[2], kernel_size=filter_size[2], stride=1, padding=padding_size[2]),
+            nn.Conv2d(layer_size[1], layer_size[2], kernel_size=filter_size[2], stride=2, padding=padding_size[2]),
             nn.BatchNorm2d(layer_size[2]),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(layer_size[2], layer_size[2], kernel_size=filter_size[2], stride=1, padding=padding_size[2]),
-            nn.BatchNorm2d(layer_size[2]),
+            nn.Conv2d(layer_size[2], layer_size[3], kernel_size=filter_size[3], stride=1, padding=padding_size[2]),
+            nn.BatchNorm2d(layer_size[3]),
             nn.ReLU(),
             nn.MaxPool2d(2),
         )
@@ -50,13 +42,13 @@ class CNNClfWithFFT(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(layer_size[1], layer_size[3], kernel_size=filter_size[2], stride=1, padding=padding_size[2]),
-            nn.BatchNorm2d(layer_size[3]),
+            nn.Conv2d(layer_size[1], layer_size[2], kernel_size=filter_size[2], stride=2, padding=padding_size[2]),
+            nn.BatchNorm2d(layer_size[2]),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(layer_size[3], layer_size[3], kernel_size=filter_size[2], stride=1, padding=padding_size[2]),
-            nn.BatchNorm2d(layer_size[3]),
+            nn.Conv2d(layer_size[2], layer_size[3] // 2, kernel_size=filter_size[3], stride=1, padding=padding_size[2]),
+            nn.BatchNorm2d(layer_size[3] // 2),
             nn.ReLU(),
             nn.MaxPool2d(2),
         )
@@ -71,18 +63,18 @@ class CNNClfWithFFT(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(layer_size[1], layer_size[3], kernel_size=filter_size[2], stride=1, padding=padding_size[2]),
-            nn.BatchNorm2d(layer_size[3]),
+            nn.Conv2d(layer_size[1], layer_size[2], kernel_size=filter_size[2], stride=2, padding=padding_size[2]),
+            nn.BatchNorm2d(layer_size[2]),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(layer_size[3], layer_size[3], kernel_size=filter_size[2], stride=1, padding=padding_size[2]),
-            nn.BatchNorm2d(layer_size[3]),
+            nn.Conv2d(layer_size[2], layer_size[3] // 2, kernel_size=filter_size[3], stride=1, padding=padding_size[2]),
+            nn.BatchNorm2d(layer_size[3] // 2),
             nn.ReLU(),
             nn.MaxPool2d(2),
         )
 
-        fc_input_size = layer_size[2] + 2*layer_size[3]
+        fc_input_size = 2*layer_size[3]
         image_size = input_size // 16
 
         self.fc = nn.Sequential(
@@ -90,15 +82,10 @@ class CNNClfWithFFT(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(256, num_classes),
-            
         )
 
     def forward(self, x):
         x_mag, x_pha = fft_2d(x)
-
-        x_mag = nn.MaxPool2d(x_mag)
-        x_pha = nn.MaxPool2d(x_pha)
-        x = nn.MaxPool2d(x)
 
         f_rgb = self.rgb_branch(x)
         f_mag = self.magnitude_branch(x_mag)
